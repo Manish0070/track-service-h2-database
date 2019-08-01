3c730c4cfcdc152fe10299a9f21f35ac1ee90b5c
Manish <manish.perumalla@cgi.com>
2019-08-01 20:13:12 +0530

    4 Methods Done

---
+++ src/main/java/com/stackroute/trackservice/controller/TrackController.java
@@ -0,0 +1,52 @@
+package com.stackroute.trackservice.controller;
+
+import com.stackroute.trackservice.domain.Track;
+import com.stackroute.trackservice.service.TrackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value="/api/v1/")
+public class TrackController {
+    private TrackService trackService;
+
+
+    @Autowired
+    public TrackController(TrackService trackService) {
+        this.trackService = trackService;
+    }
+
+
+
+    @PostMapping("track")
+    public ResponseEntity<?> setTrack(@RequestBody Track track){
+        Track savedUser = trackService.saveTrack(track);
+        System.out.println(savedUser);
+        return new ResponseEntity<>(savedUser, HttpStatus.OK);
+    }
+
+    @GetMapping("track/{id}")
+    public ResponseEntity<?>  getById(@PathVariable int id  ){
+        Track retreive = trackService.getById(id);
+        System.out.println(retreive);
+        return new ResponseEntity<>(retreive, HttpStatus.OK);
+
+    }
+    @GetMapping("track")
+    public ResponseEntity<?> getAllTracks(){
+        List<Track> getAllTrackList=trackService.getAllTracks();
+        System.out.println(getAllTrackList);
+        return new ResponseEntity<>(getAllTrackList,HttpStatus.CREATED);
+    }
+    @DeleteMapping("track/{id}")
+    public ResponseEntity<?> trackdelById(@PathVariable int id){
+        Optional<Track> deleted = trackService.trackdelById(id);
+        return new ResponseEntity<>(deleted,HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/stackroute/trackservice/service/TrackServiceImpl.java
@@ -0,0 +1,45 @@
+package com.stackroute.trackservice.service;
+
+import com.stackroute.trackservice.domain.Track;
+import com.stackroute.trackservice.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TrackServiceImpl implements TrackService {
+    private TrackRepository trackRepository;
+    @Autowired
+    public TrackServiceImpl(TrackRepository trackRepository) {
+        this.trackRepository = trackRepository;
+    }
+
+    @Override
+    public Track saveTrack(Track track) {
+        Track saveduser = trackRepository.save(track);
+        return saveduser;
+    }
+
+    @Override
+    public Track getById(int id) {
+        Track retreiveduser = trackRepository.findById(id).get();
+        return retreiveduser;
+    }
+
+    @Override
+    public List<Track> getAllTracks(){
+        List<Track> trackList=trackRepository.findAll();
+        return trackList;
+    }
+    @Override
+    public Optional<Track> trackdelById(int id){
+        Optional<Track> delete=trackRepository.findById(id);
+
+        if (delete.isPresent()){
+            trackRepository.deleteById(id);
+        }
+        return delete;
+    }
+}
